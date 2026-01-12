833cfa7fd3421a2ad67d961b9a2c37b4ec912274
manojboldvizbyte <manoj.boldvizbyte@gmail.com>
2026-01-12 16:57:02 +0530

    top navbar in dashboard commit

--- boldvizbyte-frontend/src/components/TopNavbar.jsx
+++ boldvizbyte-frontend/src/components/TopNavbar.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import "../styles/topnavbar.css";
-import logo from "../bvb.png" // make sure the path matches your logo
+import logo from "../bvb.png";
 
 const TopNavbar = () => {
   return (
-    <div className="top-navbar">
-      <img src={logo} className="top-navbar-logo" alt="company logo" />
-      <p>Welcome, Admin</p>
-    </div>
+    <nav className="top-navbar">
+      <div className="navbar-left">
+        <img src={logo} alt="Company Logo" className="top-navbar-logo" />
+        <span className="navbar-brand">Dashboard</span>
+      </div>
+      
+      <div className="navbar-right">
+        <div className="user-info">
+          <div className="user-avatar">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+              <circle cx="12" cy="7" r="4" />
+            </svg>
+          </div>
+          <span className="user-name">Admin</span>
+        </div>
+      </div>
+    </nav>
   );
 };
 
